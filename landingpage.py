4de2122ef29920fe0c8393fd5d1f2fe625f91ba4
isk-18 <isk18@georgetown.edu>
2026-03-06 16:01:37 -0500

    Add files via upload

--- landingpage.py
+++ landingpage.py
@@ -1,4 +1,13 @@
 import streamlit as st
-import pandas as pd
 
-st.write("Burbio Landing Page")
+st.set_page_config(
+    page_title="Burbio",
+    layout="centered",
+)
+
+# Logo
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("burbio_logo.png", use_container_width=True)
+
+st.divider()
